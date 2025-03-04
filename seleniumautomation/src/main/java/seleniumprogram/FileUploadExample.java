93e95f2a8d4adaedeed948f9f0ac0e7dc9fb0c6e
anexr <anexr@HP>
2025-03-04 14:21:27 +0530

    Assignment getTotal(InputFormPage)

---
+++ seleniumautomation/src/main/java/seleniumprogram/FileUploadExample.java
@@ -0,0 +1,56 @@
+package seleniumprogram;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FileUploadExample {
+	public WebDriver driver;
+
+	public void fileUpload() {
+		driver = new ChromeDriver();
+		driver.get("https://www.ilovepdf.com/pdf_to_word");
+		driver.manage().window().maximize();
+		WebElement chooseFile = driver.findElement(By.xpath("//span[text()='Select PDF file']"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		wait.until(ExpectedConditions.elementToBeClickable(chooseFile));
+		chooseFile.click();
+		chooseFile.sendKeys("C:\\Annex\\Java\\Assignments\\pdf\\Aggregation.pdf");
+	}
+
+	public void robotClass() throws AWTException {
+		driver = new ChromeDriver();
+		driver.get("https://www.ilovepdf.com/pdf_to_word");
+		driver.manage().window().maximize();
+		WebElement chooseFile = driver.findElement(By.xpath("//span[text()='Select PDF file']"));
+		chooseFile.click();
+		StringSelection stringSelection = new StringSelection("C:\\Annex\\Java\\Assignments\\pdf\\Aggregation.pdf");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);// copy path to clipboard
+		Robot robot = new Robot();
+		robot.delay(2000);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_V);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+	}
+
+	public static void main(String[] args) throws AWTException  {
+		// TODO Auto-generated method stub
+		FileUploadExample fileUploadExample = new FileUploadExample();
+		// fileUploadExample.fileUpload();
+		fileUploadExample.robotClass();
+	}
+
+}
